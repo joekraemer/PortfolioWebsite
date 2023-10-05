49a215060f22d2c038d51aca44d142b0f570ed7b
Joe Kraemer <boarder981@gmail.com>
2023-10-05 11:44:25 -0700

    Add some of the pages from the old website

---
+++ src/components/pages/CryptocurrencyTracker.js
@@ -0,0 +1,31 @@
+<>
+    <h1> Cryptocurrency Tracker </h1>
+    <p> I wrote a python script to track cryptocurreny prices as well as the profitability of my personal mining rig. </p>
+
+    <> Image </>
+
+    <p>
+    During the Spring of 2017, I became fascinated with cryptocurrencies and blockchain technologies.  I was amazed at the novelty of the decentralized authority and the public ledger.  The more I learned about the technology, the more I wanted to get involved.  
+
+    Towards the end of May, I decided that I was going to build a GPU mining rig to mine ether on the Ethereum network.  It seemed like a relatively safe investment because I could always resell or reuse the hardware once mining became unprofitable.
+
+    I spent about two weeks scouring craigslist, eBay, and internet forms searching for the cheap and relatively power efficient RX400 series GPUs.  I acquired 3 GPUs, a multi PCIe slot motherboard, a processor, a beefy 1000 watt power supply and some cheap RAM for $650.
+
+    When my dad found out how much money I had spent investing in a volatile market, he was very skeptical and wanted to see if it would end up being profitable overall.  
+
+    Initially I created an excel workbook and I was logging the current prices and my ether production manually. This was a tedious task that I really wished I didn't have to do.  I started thinking to myself and I realized that this would be a perfect opportunity to use my newly acquired Python knowledge.
+
+    I learned Python for a statistics and probability course that I took, and I really wanted to do something more with it.  I spent a lot of time learning about JSON and REST APIs.  I was fascinated that there were so many websites that allowed you to interact with them on such a simple level.  I quickly found websites that had APIs that I could access to track prices as well as the production of my mining rig.
+
+    The final script accesses 4 different websites to obtain bitcoin and Ethereum prices, how much bitcoin, Ethereum and USD I have, as well as my projected profitability.  Then this data is saved into a csv file every hour.
+
+    At the end of the summer I opened the csv in Excel and created a variety of graphs to show the results of my investment.  You can see the results below.
+
+    My dad was happy to see that my investment was a success and I am excited to see how much more the cryptocurrency community will grow.
+    </p>
+
+    <> Image </>
+    <> Image </>
+    <> Image </>
+
+</>
---
+++ src/components/pages/ULockBikeMount.js
@@ -0,0 +1,20 @@
+<>
+    <h1> U-Lock Bike Mount </h1>
+    <p> I designed and manufactured my very own custom lock mount. </p>
+
+    <> Image </>
+
+    <p> Once I bought a new bike, I decided to protect my investment with something a little more heavy duty than my old $5 cable lock from Wal-Mart.  So I purchased a hardened steel U-Lock.  I quickly found out that the U-Lock has no good place to go, unlike my old lock which I could wrap around the handle bars.  So the best spot is around the top tube and where it moves freely and somehow smashes against my knees with every pedal.  So I decided to fix it.
+
+        I did some quick research on google and found that there is relatively few products out there.  Most use a series of velcro straps or some sort of pouch.  I wanted something stable and strong, yet quick and easy to remove.  
+
+        Originally, I was going to use some sort of clasp, perhaps with some sort of snap-fit.  However, later in the day, I was inspired by the latch on our fence gate. This design would allow for quick one handed removal while keeping the lock secure (unless I were to flip over, but I think my lock falling off would be the least of my worries).
+    </p>
+
+    <> Image </>
+
+    <p> I had to use run through a couple iterations,  because I wasn't able to predict the shrinkage of the plastic in certain places, particularly the screw holes. </p>
+
+    <> Image </>
+
+</>
