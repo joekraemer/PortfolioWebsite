71dd2b5b59e04bfb924fee08afb2bffcbe05a6ac
Joe Kraemer <boarder981@gmail.com>
2023-10-05 22:53:16 -0700

    Adding more routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services'
+import Products from './components/pages/Products'
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
         </Routes>
       </Router>
     </>
